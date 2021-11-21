1516d88d74331cfb2e173516c7cff7c02b7831fa
Kris Zhao <kriszhao@comcast.net>
2021-11-21 02:53:56 -0500

    Loading svg into document

--- src/app/icon-display/icon-display.component.ts
+++ src/app/icon-display/icon-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 // The url length of the icon handler.
@@ -9,27 +9,69 @@ const URL_LENGTH = 13;
   templateUrl: './icon-display.component.html',
   styleUrls: ['./icon-display.component.css']
 })
-export class IconDisplayComponent implements OnInit {
+export class IconDisplayComponent implements OnInit, AfterViewInit {
 
-  iconUrl: string | undefined;
+  onAfterSVGSourced: boolean = false;
+
+  @ViewChild("svgImage") svgImageView!: ElementRef;
+  @ViewChild("svgObject") svgObjectView!: ElementRef;
+
+  iconUrl!: string;   //The icon url given by user
+  iconUri!: string;   //The icon asset path
   oldIcon: HTMLLinkElement | null = document.querySelector('#tab-favicon');
   imageSelector: string = "/materialicons/24px.svg";
 
+
+  //Styling
+  fillColor!: string;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private renderer: Renderer2
   ) { }
 
+  svgLoaded(){
+    if(this.onAfterSVGSourced){
+      console.warn("Loaded");
+      let styleElement = this.renderer.createElement("style", "http://www.w3.org/2000/svg");
+      let styleEntry = this.renderer.createText(":nth-child(2) { fill: " + this.fillColor + "; }");
+      console.warn(styleElement);
+      console.warn(styleEntry);
+  
+      this.renderer.appendChild(styleElement, styleEntry);
+      let svgObjectViewNE = this.svgObjectView.nativeElement;
+      let svgObject = svgObjectViewNE.contentDocument.children[0];
+      console.warn(svgObject);
+    }
+  }
 
   ngOnInit(): void {
+    console.warn("init");
+    // Find the url that the user "searched" for.
     this.iconUrl = this.router.url.slice(URL_LENGTH);
-    console.warn(this.iconUrl);
+    this.iconUri = "/assets/icons" + this.iconUrl + this.imageSelector;
 
     // If the old icon exists:
+    // if(this.oldIcon != null){
+    //   // Set the link to the url.
+    //   // this.oldIcon.href = "/assets/icons" + this.iconUrl + this.imageSelector;
+    //   this.renderer.setAttribute(this.svgObjectView.nativeElement, "data", this.iconUri);
+    // }
+  }
+
+  ngAfterViewInit(){
+    
     if(this.oldIcon != null){
       // Set the link to the url.
-      console.warn(this.oldIcon);
-      this.oldIcon.href = "/assets/icons" + this.iconUrl + this.imageSelector;
+      // this.oldIcon.href = "/assets/icons" + this.iconUrl + this.imageSelector;
+      this.renderer.setAttribute(this.svgObjectView.nativeElement, "data", this.iconUri);
+      this.onAfterSVGSourced = true;
+      //Create the svg style:
+      
     }
+    console.warn("view initiailized");
+    console.warn(this.svgImageView);
+    console.warn(this.svgObjectView);
   }
 
 }
